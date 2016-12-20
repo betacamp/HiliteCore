905d5dfd853e2b96c18c3e39e09bfa628494ff67
Preston Pope <prestonwpope@gmail.com>
2016-12-19 23:46:34 -0800

    Initial Commit

---
+++ HiliteCore/HiliteCore.h
@@ -0,0 +1,19 @@
+//
+//  HiliteCore.h
+//  HiliteCore
+//
+//  Created by Preston Pope on 12/19/16.
+//  Copyright © 2016 betacamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HiliteCore.
+FOUNDATION_EXPORT double HiliteCoreVersionNumber;
+
+//! Project version string for HiliteCore.
+FOUNDATION_EXPORT const unsigned char HiliteCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HiliteCore/PublicHeader.h>
+
+
