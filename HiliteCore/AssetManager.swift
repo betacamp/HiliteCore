ed5db557a583261bdb9accde2cf18e5e979714ea
Preston Pope <prestonwpope@gmail.com>
2017-01-06 21:19:49 -0800

    adding more sdk core

---
+++ HiliteCore/AssetManager.swift
@@ -0,0 +1,16 @@
+//
+//  AssetManager.swift
+//  HiliteCore
+//
+//  Created by Preston Pope on 1/6/17.
+//  Copyright © 2017 betacamp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+open class AssetManager {
+    open class func applicationBackgroundImage() -> UIImage! {
+        return UIImage(named: "hilite-bg-image")
+    }
+}
---
+++ HiliteCore/HLBaseViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HLBaseViewController.swift
+//  hilite
+//
+//  Created by Preston Pope on 1/6/17.
+//  Copyright © 2017 Center Camp Media LLC. All rights reserved.
+//
+
+import Foundation
